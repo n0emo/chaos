c4f226799630a28039a5b4e9a3befbb109a8edbf
Albert Shefner <dev_n0emo@tuta.io>
2024-09-21 14:02:28 +0300

    Add todos

--- src/script.js
+++ src/script.js
@@ -1,3 +1,13 @@
+// TODO: weapons
+// TODO: HP
+// TODO: explosion
+// TODO: bonuses (healing, invulnerability, bullet clean, weapon upgrade)
+// TODO: enemies
+// TODO: events (fishing, ads, unknown, unknown)
+// TODO: menu
+// TODO: tutoial
+// TODO: main level
+// TODO: final boss
 import { Game } from "./game.js"
 
 const game = Game.load()
